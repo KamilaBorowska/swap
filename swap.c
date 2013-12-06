d5766ea1052d5b57a28794b44a6bc3b14019aad8
Konrad Borowski <glitchmr@myopera.com>
2013-12-06 20:08:36 +0100

    Make swap work under C89.

--- swap.c
+++ swap.c
@@ -1,9 +1,24 @@
 #include <string.h>
 
 #define SWAP(a, b) swap_internal(&(a), &(b), sizeof *(1 ? &(a) : &(b)))
-static inline void swap_internal(void *a, void *b, size_t size) {
+
+#if __STDC_VERSION__ < 199901L
+/* For malloc, not needed in C99 */
+#include <stdlib.h>
+#else
+/* Inline only works in C99 compilers. */
+inline
+#endif
+static void swap_internal(void *a, void *b, size_t size) {
+#if __STDC_VERSION__ < 199901L
+    char *tmp = malloc(size);
+#else
     char tmp[size];
+#endif
     memcpy(tmp, a, size);
     memmove(a, b, size);
     memcpy(b, tmp, size);
+#if __STDC_VERSION__ < 199901L
+    free(tmp);
+#endif
 }

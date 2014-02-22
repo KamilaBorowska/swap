4a6210e2a3a075b4e684837a91b6653cffcc9688
Konrad Borowski <glitchmr@myopera.com>
2014-02-22 21:57:23 +0100

    Use algorithm that doesn't allocate the memory it doesn't need.
    This makes the swap usable with huge objects.

--- swap.h
+++ swap.h
@@ -3,13 +3,28 @@
 
 #define SWAP(a, b) swap_internal(&(a), &(b), sizeof *(1 ? &(a) : &(b)))
 
+#if __STDC_VERSION__ >= 199901L
 /* Inline only works in C99 compilers. */
 inline
 #endif
-static void swap_internal(void *a, void *b, size_t size) {
-    char *tmp = malloc(size);
-    memcpy(tmp, a, size);
-    memmove(a, b, size);
-    memcpy(b, tmp, size);
-    free(tmp);
+static void swap_internal(void *a_ref, void *b_ref, size_t size) {
+    size_t i;
+    /*
+     * Such rather strange loop is caused by the fact that it has to be
+     * able to iterate is by maximum size_t size, even if unlikely.
+     */
+    char *a = a_ref;
+    char *b = b_ref;
+    for (i = 0; i != size; i++) {
+        char tmp = *a;
+        *a = *b;
+        *b = tmp;
+
+        /*
+         * Pointer arithmetic, in order to not introduce signed
+         * integers into math.
+         */
+        a++;
+        b++;
+    }
 }
